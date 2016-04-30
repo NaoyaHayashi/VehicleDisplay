6e128d15c1db7f8eaf6a16c16ff6769dcd9b9147
Naoya Hayashi <nhayashi@sfu.ca>
2016-04-30 14:28:03 -0700

    Minor fix

--- VehicleViewer/src/VehicleViewer.java
+++ VehicleViewer/src/VehicleViewer.java
@@ -1,8 +1,5 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import javax.swing.JFrame;
-import javax.swing.Timer;
 
 /**
    VehicleViewer class is a tester class for VehicleComponent, Vehicle, Car and Truck classes. <br>
